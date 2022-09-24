9470cba5b32de1e30fa42f9caab7cfb7c7f89bc3
Sachin Srivastav <114273443+sachinjnp@users.noreply.github.com>
2022-09-24 09:27:45 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,4 +1,4 @@
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
