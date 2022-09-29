4d44891a1a6093163f34b0af2d97b8648e0f9eac
Sachin Srivastav <114273443+sachinjnp@users.noreply.github.com>
2022-09-29 15:38:53 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -30,10 +30,10 @@ streamlit.header("Fruityvice Fruit Advice!")
   #if not fruit_choice :
     #stremlit.error("Please select a fruit to get details");
    # else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
       #streamlit.text(fruityvice_response.json())
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized);
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized);
       
 
 
