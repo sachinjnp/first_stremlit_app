10b0cec69b2827805dff25f0573dbe012717af9e
Sachin Srivastav <114273443+sachinjnp@users.noreply.github.com>
2022-09-29 16:09:05 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -54,7 +54,9 @@ streamlit.write('The user entered ', fruit_choice);
 if not fruit_choice:
   stremlit.error("Please select a fruit to get details")
 else:
-  streamlit.text("Omega I am ")
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized);
   
 
 
