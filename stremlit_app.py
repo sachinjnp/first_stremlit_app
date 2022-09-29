d359b4bf36118797b37cb1edde7327512418c59e
Sachin Srivastav <114273443+sachinjnp@users.noreply.github.com>
2022-09-29 15:27:05 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -30,10 +30,14 @@ try:
   if not fruitchoice :
     stremlit.error("Please select a fruit to get details");
     else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json());
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      #streamlit.text(fruityvice_response.json());
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      
+
+
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
