d0aa2c05a33ca5829a145cb8bd89f646af92cfd5
Sachin Srivastav <114273443+sachinjnp@users.noreply.github.com>
2022-09-29 15:23:33 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -26,21 +26,25 @@ streamlit.dataframe(fruits_to_show)
 #import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-#streamlit.text(fruityvice_response.json());
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-#streamlite.stop();
 try:
   if not fruitchoice :
     stremlit.error("Please select a fruit to get details")
     else:
-fruit_choice = streamlit.text_input('What fruit would you like information about?',fruit_choice)
-streamlit.write('The user entered ', fruit_choice);
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#streamlit.text(fruityvice_response.json());
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error();
+#streamlite.stop();
+
+    
+fruit_choice = streamlit.text_input('What fruit would you like information about?',"Kiwi")
+streamlit.write('The user entered ', fruit_choice);
+
 
 #import snowflake.connector;
 
